89361c4c4502955c9e7630e3958f3a29c88abdfa
zhng1456 <410814938@qq.com>
2018-03-19 09:38:47 +0800

    update

--- src/com/wang/core/TankClient.java
+++ src/com/wang/core/TankClient.java
@@ -3,13 +3,18 @@ package com.wang.core;
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame{
+	public static final int GAME_WIDTH=800;
+	public static final int GAME_HEIGHT=600;
 	//坐标
 	int x=50;
 	int y=50;
+	//利用缓冲解决闪烁的问题 
+	Image offScreenImage=null;
 	//每隔一段时间重画一次，让tank移动,用线程来实现
 	private class PaintThread implements Runnable{
 		@Override
@@ -17,9 +22,9 @@ public class TankClient extends Frame{
 			// TODO Auto-generated method stub
 			//调用的是外部包装类的repaint
 			while(true){
-				repaint();
+				repaint();//repaint先调用update，再调用paint
 				try {
-					Thread.sleep(50);
+					Thread.sleep(20);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -38,9 +43,24 @@ public class TankClient extends Frame{
 		g.setColor(c);
 		y+=5;
 	}
+	@Override
+	public void update(Graphics g) {
+		if(offScreenImage==null){
+			offScreenImage=this.createImage(GAME_WIDTH,GAME_HEIGHT);
+		}
+		//取到图片的Graphics对象
+		Graphics gOffScreen=offScreenImage.getGraphics();
+		Color c=gOffScreen.getColor();
+		gOffScreen.setColor(Color.GREEN);
+		gOffScreen.fillRect(0,0,GAME_WIDTH,GAME_HEIGHT);
+		//恢复颜色
+		gOffScreen.setColor(c);
+		paint(gOffScreen);
+		g.drawImage(offScreenImage,0,0,null);
+	}
 	public void lauchFrame(){
 		this.setLocation(400,300);
-		this.setSize(800,600);
+		this.setSize(GAME_WIDTH,GAME_HEIGHT);
 		this.setTitle("tankWar");
 		//设置监听事件，用匿名类来实现
 		this.addWindowListener(new WindowAdapter(){
@@ -49,10 +69,10 @@ public class TankClient extends Frame{
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);//直接退出 
 			}
-			
 		});
 		//不允许改变窗口大小
 		this.setResizable(false);
+		this.setBackground(Color.GREEN);
 		setVisible(true);
 		//启动线程
 		new Thread(new PaintThread()).start();
