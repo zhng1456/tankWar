24456a344ffde9f280c9eba548c3651c009c3fe0
zhng1456 <410814938@qq.com>
2018-04-07 19:06:50 +0800

    transfer udp port

--- src/com/wang/core/NetClient.java
+++ src/com/wang/core/NetClient.java
@@ -1,20 +1,42 @@
 package com.wang.core;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class NetClient {
+	//UDP端口的起始端口
+	private static int UDP_PORT_START=2223;
+	private int udpPort;
+	public NetClient(){
+		udpPort=UDP_PORT_START++;
+	}
 	public void connect(String IP,int port){
+		Socket s=null;
 		try {
-			Socket s = new Socket(IP,port);
+			s = new Socket(IP,port);
 			System.out.println("Connected to Server");
+			//将这个客户端的UDP端口号发送过去
+			DataOutputStream dos=new DataOutputStream(s.getOutputStream());
+			dos.writeInt(udpPort);
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally{
+			if(s!=null){
+				try {
+					s.close();
+					s=null;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
+	
 }
--- src/com/wang/core/TankServer.java
+++ src/com/wang/core/TankServer.java
@@ -1,8 +1,11 @@
 package com.wang.core;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 服务器类
@@ -14,19 +17,43 @@ public class TankServer {
 	 * TCP监听端口
 	 */
 	public static final int TCP_PORT=8888;
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//一系列连上来的客户端
+	List<Client> clients=new ArrayList<Client>();
+	public void start(){
 		try {
 			ServerSocket ss=new ServerSocket(TCP_PORT);
 			while(true){
 				Socket s=ss.accept();
 				System.out.println("A Client Connect Addr:"+s.getInetAddress()+":"+s.getPort());
+				DataInputStream dis=new DataInputStream(s.getInputStream());
+				//接受客户端发来的udp端口
+				int udpPort=dis.readInt();
+				String ip=s.getInetAddress().getHostAddress();
+				Client c=new Client(ip, udpPort);
+				clients.add(c);
+				s.close();
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new TankServer().start();
+	}
+	/**
+	 * 内部类，保留连接上来的客户端的基本信息
+	 * @author Administrator
+	 *
+	 */
+	private class Client{
+		String IP;
+		int udpPort;
+		public Client(String iP, int udpPort) {
+			IP = iP;
+			this.udpPort = udpPort;
+		}
+		
+	}
 }
